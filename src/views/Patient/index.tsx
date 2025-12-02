b8e3776ffb454293bb4f1bdd15949b2adc1add19
Waqas Aziz <88161315+MohammadWaqasAziz@users.noreply.github.com>
2025-12-02 17:03:38 +0500

    Stage (#115)
    * Feature/multiple calls (#111)
    
    * change the position of audiowave form "sticky" , and change the api reponse
    
    * add the filter of calls, by default the latest call is selected
    
    * fix the issue of making marker based on unrelated slots
    
    * remove the comments
    
    * Feature/patient filters (#112)
    
    * change the name of pharmancy screen to patient screen and handle the null time stamp
    
    * add the filters and also enable the search by name or member ID
    
    * fix the ont size of call_type and payer_name (#113)
    
    * change the limit to 100

--- src/views/Patient/index.tsx
+++ src/views/Patient/index.tsx
@@ -27,7 +27,7 @@ const Patient = () => {
 
   useEffect(() => {
     triggerFetch({
-      limit: 50,
+      limit: 100,
       search: debouncedSearch?.trim() || undefined,
       call_type: callType.length > 0 ? callType.join(",") : undefined,
       payer_name: payerName.length > 0 ? payerName.join(",") : undefined,
